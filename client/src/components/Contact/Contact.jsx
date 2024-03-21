10059bb31da738482e9b6e51fc04aaba5b6716d1
Garg-Ishita <115682492+Garg-Ishita@users.noreply.github.com>
2024-03-22 00:15:22 +0530

    Update Contact.jsx

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -82,11 +82,11 @@ const Contact = () => {
           </div>
         </div>
         {/* right side */}
-        <div className="c-right">
+{/*         <div className="c-right">
           <div className="image-container">
             <img src="./contact.jpg" alt="" />
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
